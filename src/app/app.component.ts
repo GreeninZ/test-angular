5ad5a45bb054afcc60c59d452e5a71e506d97bfa
GreeninZ <93052932+GreeninZ@users.noreply.github.com>
2023-01-11 10:26:01 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  player: string;
+  getValue(val) {
+    this.player = val;
+  }
 }
